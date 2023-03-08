a1ae55a1d988bdceac62d2ccb2662754bbff4cbc
JeanCarlosDelai <112594276+JeanCarlosDelai@users.noreply.github.com>
2023-03-07 21:50:19 -0300

    Criptografia da senha

--- Aula2/boasVindas.php
+++ Aula2/boasVindas.php
@@ -4,6 +4,12 @@ require('verificaAutenticacao.php');
 //ola
 echo 'Login efetuado com sucesso';
 
+$senha_cripto = password_hash(123, PASSWORD_BCRYPT, [
+    'cost' => 11,
+]);
+echo $senha_cripto;
+die();
+
 ?>
 <P>
     <a href="logout.php">Sair</a>
--- Aula2/form_receber.php
+++ Aula2/form_receber.php
@@ -1,9 +1,14 @@
 <?php
     # form_receber.php
+
+$senha_cripto = '$2y$11$IZ.KvE2HfwZEr6UxfpSWGOvhoYqychUBmLneTkWx/1oiC/hlJmABm';
+
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    if ($usuario == 'Jean' && $senha == '123') {
+    if ($usuario == 'Jean'
+    &&
+    password_verify($senha, $senha_cripto)){ 
         // Autenticação OK
         session_start();
         $_SESSION['usuario'] = $usuario;
